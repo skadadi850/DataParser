c87cef4a98efdadeefece6d72477c07eee67e147
Simran Kadadi <simrandkk@gmail.com>
2019-03-09 13:18:45 -0800

    create State class

---
+++ src/State.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class State {
+    private String name;
+    private List<County> counties;
+
+    public State(String name) {
+        this.name = name;
+        counties = new ArrayList<County>();
+    }
+
+    public void addCounty (County c){
+        counties.add(c);
+    }
+
+    public void removeCounty (County c){
+        counties.remove(c);
+    }
+
+    public County removeCounty (int index){
+        return counties.remove(index);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<County> getCounties() {
+        return counties;
+    }
+
+    public void setCounties(List<County> counties) {
+        this.counties = counties;
+    }
+
+}
