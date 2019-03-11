7649b80d0172fd3aa8fb61a14e4947c7e14f046c
Simran Kadadi <simrandkk@gmail.com>
2019-03-10 22:45:50 -0700

    Test parsing of Education.csv in main

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,33 @@ public class Main {
     public static void main(String[] args) {
         //Test of Utils
 
-        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
-        ArrayList<ElectionResult> results = Utils.parse2016ElectionResults(data);
+//        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+//        ArrayList<ElectionResult> results = Utils.parse2016ElectionResults(data);
+//
+////         test toString method
+//        for (int i = 0; i<results.size(); i++) {
+//            System.out.println(results.get(i).toString());
+//        }
 
-        // test toString method
-        for (int i = 0; i<results.size(); i++) {
-            System.out.println(results.get(i).toString());
+        String data1 = Utils.readFileAsString("data/Education.csv");
+        String data2 = Utils.readFileAsString("data/Unemployment.csv");
+
+        ArrayList<Education2016> results1 = Utils.parseEducationResults(data1);
+
+        for (int i = 0; i < results1.size(); i++){
+            System.out.println(results1.get(i).getBachelorsOrMore());
         }
+
+
+
+//        DataManager structure = Utils.parseAllData(data,data1,data2);
+//
+//        System.out.println (structure.getStates().size());
+//
+//        for (int i = 0; i < structure.getStates().size(); i++){
+//            System.out.println (structure.getStates().get(i));
+//        }
+
 
     }
 }
