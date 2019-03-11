9db4931953c504d579d59c050cc4003caf4c2df3
Simran Kadadi <simrandkk@gmail.com>
2019-03-10 22:44:36 -0700

    Create parseEducationResults and removeQuoteFromRow methods in Utils.java to format Education.csv

--- src/Utils.java
+++ src/Utils.java
@@ -23,10 +23,10 @@ public class Utils {
     public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
         ArrayList <ElectionResult> results = new ArrayList<>();
 
-        String [] rows = data.split("\n");
-        for (int i = 1; i < rows.length; i++){
+            String [] rows = data.split("\n");
+            for (int i = 1; i < rows.length; i++){
 
-            String countyData = rows [i];
+                String countyData = rows [i];
             String [] fields = parseDifference (countyData);
 
             double demVotes = Double.parseDouble(fields[1]);
@@ -52,15 +52,15 @@ public class Utils {
 
     private static String[] parseDifference(String data) {
         String [] fields;
-        String finalString;
+        String finalString = "";
+
 
         int indexOfFirstQuote = data.indexOf("\"");
 
         if (indexOfFirstQuote== -1){
             fields = data.split(",");
         }else {
-
-            finalString = removeCommasFromDifference (data);
+            finalString = removeCommasFromDifference(data);
             fields = finalString.split(",");
         }
         return fields;
@@ -94,40 +94,36 @@ public class Utils {
         String[] rows = educationData.split("\n");
 
         for (int i = 6; i < 3288; i++) {
-            String[] fields = educationData.split(",");
+
+            String[] fields = rows[i].split(",");
 
             String stateName = fields[1];
-            boolean stateExists = false;
-            for (int j = 0; j < fields.length; j++) {
-                if (fields[j].equals(stateName)) {
-                    stateExists = true;
-                }
-            }
-
-            if (stateExists == false) {
-                State state = new State(stateName);
-                structure.addState(state);
-
-                String countyName = fields[2];
-                int fips = Integer.parseInt(fields[0]);
-
-                double[] electionResults = getElection2016Data(electionData, countyName);
-                Election2016 vote2016 = new Election2016(electionResults[0], electionResults[1], electionResults[2]);
-
-                double[] educationResults = getEducation2016Data(educationData, countyName);
-                Education2016 educ2016 = new Education2016(educationResults[0], educationResults[1], educationResults[2],
-                        educationResults[3]);
+            System.out.println (stateName);
 
 
-                String[] employmentResults = getEmployment2016Data(employmentData, countyName);
-                Employment2016 employ2016 = new Employment2016(Integer.parseInt(employmentResults[0]),
-                        Integer.parseInt(employmentResults[1]), Integer.parseInt(employmentResults[2]),
-                        Double.parseDouble(employmentResults[3]));
+            State state = new State(stateName);
+            structure.addState(state);
 
-                County c = new County(countyName, fips, vote2016, educ2016, employ2016);
+            String countyName = fields[2];
+            int fips = Integer.parseInt(fields[0]);
 
-                state.addCounty(c);
-            }
+            double[] electionResults = getElection2016Data(electionData, countyName);
+            Election2016 vote2016 = new Election2016(electionResults[0], electionResults[1], electionResults[2]);
+
+            double[] educationResults = getEducation2016Data(educationData, countyName);
+            Education2016 educ2016 = new Education2016(educationResults[0], educationResults[1], educationResults[2],
+                    educationResults[3]);
+
+
+            String[] employmentResults = getEmployment2016Data(employmentData, countyName);
+            Employment2016 employ2016 = new Employment2016(Integer.parseInt(employmentResults[0]),
+                    Integer.parseInt(employmentResults[1]), Integer.parseInt(employmentResults[2]),
+                    Double.parseDouble(employmentResults[3]));
+
+            County c = new County(countyName, fips, vote2016, educ2016, employ2016);
+
+            state.addCounty(c);
+
 
         }
         return structure;
@@ -177,9 +173,17 @@ public class Utils {
 
         for (int i = 6; i < rows.length; i++) {
             String eduRow = rows[i];
+
+
             String [] fields = parseDifference(eduRow);
+            for (int k = 0; k < fields.length; k++){
+                System.out.println(fields[k]);
+            }
+
+
 
             if (fields[2].equals(countyName)){
+
                 noHighSchool = Double.parseDouble(fields[fields.length-8]);
                 onlyHighSchool = Double.parseDouble(fields[fields.length-7]);
                 someCollege = Double.parseDouble(fields[fields.length-6]);
@@ -218,12 +222,71 @@ public class Utils {
                 votingInfo[2] = Double.parseDouble(totalVotes);
             }
 
-            return votingInfo;
+
         }
 
-
+        return votingInfo;
 
     }
 
+    private static String removeCommasFromDifference(String data, int index) {
+        String strWODifferenceB = "";
+        String strWODifferenceE = "";
 
+        int indexOfFirstQuote = index;
+        int indexOfSecondQuote = data.indexOf("\"", indexOfFirstQuote+1);
+
+        String difference = data.substring(indexOfFirstQuote+1,indexOfSecondQuote);
+        strWODifferenceB = data.substring(0,indexOfFirstQuote);
+        strWODifferenceE = data.substring(indexOfSecondQuote+1);
+
+        int indexOfCommaInQuote = difference.indexOf(",");
+
+        while (indexOfCommaInQuote != -1) {
+            difference = difference.substring(0,indexOfCommaInQuote) + difference.substring(indexOfCommaInQuote+1);
+            indexOfCommaInQuote = difference.indexOf(",");
+        }
+
+        return (strWODifferenceB + difference + strWODifferenceE);
+    }
+
+
+    private static String[] removeQuoteFromRow (String data) {
+        String [] fields;
+        String finalString = data;
+
+        int indexOfFirstQuote = data.indexOf("\"");
+        if (indexOfFirstQuote== -1){
+            fields = data.split(",");
+        }else {
+
+            while (indexOfFirstQuote != -1) {
+                finalString = removeCommasFromDifference(finalString,indexOfFirstQuote);
+                indexOfFirstQuote = finalString.indexOf("\"");
+            }
+            fields = finalString.split(",");
+        }
+        return fields;
+    }
+
+    public static ArrayList<Education2016> parseEducationResults(String data1) {
+
+        ArrayList <Education2016> results = new ArrayList<>();
+
+        String [] rows = data1.split("\n");
+        for (int i = 6; i < 3288; i++){
+
+            String [] fields = removeQuoteFromRow (rows[i]);
+
+            double noHighSchool = Double.parseDouble(fields[fields.length-8]);
+            double onlyHighSchool = Double.parseDouble(fields[fields.length-7]);
+            double someCollege = Double.parseDouble(fields[fields.length-6]);
+            int bachelorsOrMore = Integer.parseInt(fields[fields.length-5]);
+
+            Education2016 stat = new Education2016(noHighSchool, onlyHighSchool, someCollege, bachelorsOrMore);
+
+            results.add(stat);
+        }
+        return results;
+    }
 }
